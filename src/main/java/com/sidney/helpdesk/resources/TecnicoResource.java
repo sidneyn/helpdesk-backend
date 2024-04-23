f85674e2516aa9a17362dfa2bff6f6ea598762a3
sidneyn <sidneyn@gmail.com>
2024-04-22 22:20:52 -0300

    endpoint create para Tecnico

--- src/main/java/com/sidney/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/sidney/helpdesk/resources/TecnicoResource.java
@@ -1,5 +1,6 @@
 package com.sidney.helpdesk.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,12 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sidney.helpdesk.domain.Tecnico;
 import com.sidney.helpdesk.domain.dtos.TecnicoDTO;
 import com.sidney.helpdesk.services.TecnicoService;
+
+import net.bytebuddy.asm.Advice.Exit;
 
 @RestController
 @RequestMapping(value = "/tecnicos")
@@ -34,6 +40,13 @@ public class TecnicoResource {
 		List<Tecnico> list = service.findAll();
 		List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}	
+
+	@PostMapping
+	public ResponseEntity<TecnicoDTO> create(@RequestBody TecnicoDTO objDTO) {
+		Tecnico newObj = service.create(objDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
-	
+		
 }
--- src/main/java/com/sidney/helpdesk/services/TecnicoService.java
+++ src/main/java/com/sidney/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sidney.helpdesk.domain.Tecnico;
+import com.sidney.helpdesk.domain.dtos.TecnicoDTO;
 import com.sidney.helpdesk.repositories.TecnicoRepository;
 import com.sidney.helpdesk.services.exceptions.ObjectnotFoundException;
 
@@ -23,6 +24,12 @@ public class TecnicoService {
 
 	public List<Tecnico> findAll() {		 
 		return repository.findAll();
+	}
+	
+	public Tecnico create(TecnicoDTO objDTO) {	
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico (objDTO);
+		return repository.save(newObj); // chamada assincrona 
 	}
 
 }
